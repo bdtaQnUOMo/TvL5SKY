0c4d943452b7c9b56ded003fd83b3d9336bc5c03
bdtaQnUOMo <114381442+bdtaQnUOMo@users.noreply.github.com>
2025-06-15 11:17:40 +0100

    解决了 空指针异常 Bug

---
+++ src/config/constants.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    userType: {
+        LEARNER: "learner",
+        INSTRUCTOR: "instructor",
+        ADMIN: "admin"
+    },
+    env: {
+        CLIENT_URL: "http://localhost:8080"
+    }
+}
